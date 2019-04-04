d8c8c30a9f0322be3a6b9c413f5000b3b7d72138
Nathaniel Beckstead <becksteadn@gmail.com>
2019-04-04 18:21:55 -0400

    add reactive title

--- first-vue/function.js
+++ first-vue/function.js
@@ -4,3 +4,10 @@ new Vue({
       message: 'Hello Vue.js!'
     }
   })
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  })
